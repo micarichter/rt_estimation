73173e3d9a6daf14b8a3502a8248a7b55f5fc786
Micaela Richter <micaelarichter@Micaelas-MacBook-Air.local>
2026-06-29 16:09:29 -0400

    updated for log odes

--- eon_diy.R
+++ eon_diy.R
@@ -5,7 +5,7 @@ source("eon_analysis.R")
 n_sens <- 1500
 
 # human sensor network from EoN
-eon_dsa <- read.csv("/Users/micaelarichter/Library/CloudStorage/OneDrive-TheOhioStateUniversity/python/configeon_dsa_deg100_r04.csv")
+eon_dsa <- read.csv("/Users/micaelarichter/Library/CloudStorage/OneDrive-TheOhioStateUniversity/python/configeon_dsa_deg100_r02.csv")
 
 # tidy the data:
 # fix initial cases: etimes = 0
@@ -32,34 +32,35 @@ names(eon_dsa) <- c("X", "id", "eTime", "iTime", "rTime", "estat", "istat", "rst
 eon_sample <- eon_dsa[sample(nrow(eon_dsa), n_sens), ]
 
 # true Rt data
-true_sim <- read.csv("/Users/micaelarichter/Library/CloudStorage/OneDrive-TheOhioStateUniversity/python/config_deg100_r04.csv")
+true_sim <- read.csv("/Users/micaelarichter/Library/CloudStorage/OneDrive-TheOhioStateUniversity/python/config_deg100_r02.csv")
 
 # Cori estimates
-cori_gt <- read.csv("/Users/micaelarichter/Library/CloudStorage/OneDrive-TheOhioStateUniversity/python/cori_config_pairs_deg100_r04.csv")
-cori_incidence <- read.csv("/Users/micaelarichter/Library/CloudStorage/OneDrive-TheOhioStateUniversity/python/config_incidence_deg100_r04.csv")
+cori_gt <- read.csv("/Users/micaelarichter/Library/CloudStorage/OneDrive-TheOhioStateUniversity/python/cori_config_pairs_deg100_r02.csv")
+cori_incidence <- read.csv("/Users/micaelarichter/Library/CloudStorage/OneDrive-TheOhioStateUniversity/python/config_incidence_deg100_r02.csv")
 names(cori_incidence) <- c("time", "incidence")
 
 # find point at which S = 0.9
 s_prop <- 0.9
 c_cutoff <- min(true_sim$time[true_sim$S/n_tot == s_prop])
-
 cori_res <- cori_est(cori_gt, cori_incidence, c_cutoff)
 
 
 # one window size
-system.time(res4 <- eon_est(dat = eon_sample, begin = 10, end = 30, width = 4, 
+system.time(res2 <- eon_est(dat = eon_sample, begin = 40, end = 100, width = 6, 
                             step = 1, obs_end = TRUE, use_empEIRsurv = TRUE))
 
 # several windows smoothed
-windows_deg100_4 <- adaptive_smooth1(c(4, 6, 8, 10))
+short <- adaptive_smooth1(c(2, 4, 6))
+long <- adaptive_smooth1(c(8, 10, 12))
+
 
 # plot
-adaptive_smooth_plot(DSA = windows_deg10_4[[3]], Cori = cori_res, truth = true_sim, R0 = 4,
+adaptive_smooth_plot(DSA = windows_deg10_4a[[3]], Cori = cori_res, truth = true_sim, R0 = 4,
                      pop = n_sens, ymax = 5)
 
 
+windows_deg100_2 <- adaptive_smooth1(c(4, 6, 8, 10))
 
-
-
-
+adaptive_smooth_plot(DSA = windows_deg100_2[[3]], Cori = cori_res, truth = true_sim, R0 = 3,
+                     pop = n_sens, ymax = 4)
 
